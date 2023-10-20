e5852c6551ee2344152ae10105df054b697db99d
Edgar Sanchez <egsanchez530@gmail.com>
2023-10-20 16:53:41 -0700

    Added function to play a round.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Initialize Variables
 
-let isGameOver = false;
-let round = 1;
-let player1Score = 0;
-let player2Score = 0;
+// let isGameOver = false;
+// let round = 1;
+// let player1Score = 0;
+// let player2Score = 0;
 let options = ['rock', 'paper', 'scissors'];
 
 // Get computers Choice from an array that includes rock, paper and scissors.
@@ -12,3 +12,29 @@ const getComputerChoice = () => {
   const compSelection = options[Math.floor(Math.random() * 3)];
   return compSelection;
 };
+
+let computerSelection = getComputerChoice();
+//  Prompting user to choose between options
+
+let p1Choice = prompt('Choose either Rock Paper or Scissors');
+p1Choice = p1Choice.toLowerCase();
+
+// Playing the game
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (
+    (computerSelection === 'rock' && playerSelection === 'scissors') ||
+    (computerSelection === 'paper' && playerSelection === 'rock') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper')
+  ) {
+    return `You lose ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return 'draw';
+  }
+}
