a46a7999befd4a59d1bcc725fa6c18814d442396
Edgar Sanchez <egsanchez530@gmail.com>
2023-10-20 16:31:32 -0700

    Added H1 to html doc. Added function for computer choice between rock, paper, and scissors.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// Initialize
+// Initialize Variables
+
+let isGameOver = false;
+let round = 1;
+let player1Score = 0;
+let player2Score = 0;
+let options = ['rock', 'paper', 'scissors'];
+
+// Get computers Choice from an array that includes rock, paper and scissors.
+
+const getComputerChoice = () => {
+  const compSelection = options[Math.floor(Math.random() * 3)];
+  return compSelection;
+};
